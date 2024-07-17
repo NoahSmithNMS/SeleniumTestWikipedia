8062e4f9b77a0fbe4cf560f4d83796f4d5ccf6f1
NoahSmith15 <noahsmith.nms@gmail.com>
2024-07-17 00:08:53 -0400

    first commit

---
+++ main/tests/findSeleniumPage.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://www.wikipedia.org")
+
+searchbox = driver.find_element(By.ID, "searchInput")
+searchbutton = driver.find_element(By.CLASS_NAME, "pure-button-primary-progressive")
+
+searchbox.send_keys("Selenium")
+searchbutton.click()
+
+text = driver.find_element(By.CLASS_NAME, "mw-page-title-main").text
+
+print(text)
